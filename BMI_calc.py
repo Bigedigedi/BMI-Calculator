39b59a6023f7ceaf2a417e10655375fe6adec000
bigedigedi <david.zimmermann@mars.uni-freiburg.de>
2023-06-29 13:58:33 +0200

    changed .py

--- BMI_calc.py
+++ BMI_calc.py
@@ -1,67 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "id": "mathematical-pillow",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Please give your size in cm 185\n",
-      "Please give your weight in kg 95\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Your BMI is 27.8 .You are overweight\n"
-     ]
-    }
-   ],
-   "source": [
-    "size = float(input(\"Please give your size in cm\"))\n",
-    "weight = float(input(\"Please give your weight in kg\"))\n",
-    "\n",
-    "def bmi_calc(size,weight):\n",
-    "    size_in_m = size/100\n",
-    "    bmi = weight/(size_in_m**2)\n",
-    "    \n",
-    "    if bmi < 18.5:\n",
-    "        print(\"Your BMI is\",round(bmi,1),\".You are Underweight\")\n",
-    "    elif bmi < 25:\n",
-    "        print(\"Your BMI is\",round(bmi,1),\".You are normalweight\")\n",
-    "    elif bmi < 30:\n",
-    "        print(\"Your BMI is\",round(bmi,1),\".You are overweight\")\n",
-    "    else:\n",
-    "        print(\"Your BMI is\",round(bmi,1),\".You have adipositas\")\n",
-    "\n",
-    "bmi_calc(size,weight)"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+size = float(input("Please give your size in cm"))
+weight = float(input("Please give your weight in kg"))
+
+def bmi_calc(size,weight):
+    size_in_m = size/100
+    bmi = weight/(size_in_m**2)
+    
+    if bmi < 18.5:
+        print("Your BMI is",round(bmi,1),".You are Underweight")
+    elif bmi < 25:
+        print("Your BMI is",round(bmi,1),".You are normalweight")
+    elif bmi < 30:
+        print("Your BMI is",round(bmi,1),".You are overweight")
+    else:
+        print("Your BMI is",round(bmi,1),".You have adipositas")
+
+bmi_calc(size,weight)
+
